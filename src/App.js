e0d503d7722cc0ff54cfb5894b44699131837f9e
vkrakeshjangid@gmail.com <121666589+rakeshjan9090@users.noreply.github.com>
2025-11-21 07:44:38 +0530

    components and pages add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Portfolio from 'src\components\Portfolio.js';
+import Portfolio from './components/Portfolio.js';
 
 function App() {
   return (
